4cc70df57f0f3cfbfb37ecd3b90937043c589e6c
Benjamin Letellier <letellier.benjamin@gmail.com>
2023-02-07 16:07:30 +0100

    CPM-952: switch @PSR-12 to @PER (PHP Evolving Recommendation)

--- components/identifier-generator/back/tests/.php_cs.php
+++ components/identifier-generator/back/tests/.php_cs.php
@@ -10,7 +10,7 @@ return (new PhpCsFixer\Config())
         'no_unused_imports' => true,
         'blank_line_before_statement' => true,
         'declare_strict_types' => true,
-        '@PSR12' => true,
+        '@PER' => true,
         'no_extra_blank_lines' => true,
         'trailing_comma_in_multiline' => true,
         'void_return' => true,
