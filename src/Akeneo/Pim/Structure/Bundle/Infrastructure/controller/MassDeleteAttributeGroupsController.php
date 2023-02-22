5bc987eddcfe87b700d844b392f6bd3fd279d0d4
anthony-grandin <anthony.grandin@getakeneo.com>
2023-02-22 11:09:59 +0100

    RAB-1285: create controller

---
+++ src/Akeneo/Pim/Structure/Bundle/Infrastructure/controller/MassDeleteAttributeGroupsController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright 2023 Akeneo SAS (https://www.akeneo.com)
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Akeneo\Pim\Structure\Bundle\Infrastructure\controller;
+
+final class MassDeleteAttributeGroupsController
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
